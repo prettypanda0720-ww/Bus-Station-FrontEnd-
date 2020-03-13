e60f3333d20d88d4202151091dd52a6d4948a24c
prettypanda0720-ww <riverstar1992@gmail.com>
2020-03-13 17:47:05 +0100

    modify index.html

--- js/main.js
+++ js/main.js
@@ -2,12 +2,12 @@ $(document).ready(function() {
   "use strict";
 
   var window_width = $(window).width(),
-    window_height = window.innerHeight,
-    header_height = $(".default-header").height(),
-    header_height_static = $(".site-header.static").outerHeight(),
-    fitscreen = window_height - header_height;
+  window_height = window.innerHeight,
+  header_height = $(".default-header").height(),
+  header_height_static = $(".site-header.static").outerHeight(),
+  fitscreen = window_height - header_height;
 
-  $(".fullscreen").css("height", window_height / 2);
+  $(".fullscreen").css("height", window_height / 2 + header_height);
   $(".fitscreen").css("height", fitscreen);
 
   if (document.getElementById("default-select")) {
