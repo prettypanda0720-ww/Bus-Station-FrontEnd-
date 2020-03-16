c0a98016d07d73eefa2978ee57843a7c259bfa58
prettypanda0720-ww <riverstar1992@gmail.com>
2020-03-16 19:09:00 +0100

    implement review-01, email page

--- js/review.js
+++ js/review.js
@@ -18,7 +18,19 @@ $(document).ready(function() {
   $('#review_section').css("height",window_height - header_height * 2);
   $('#review_detail_home').css("height",window_height - header_height * 4 - 30);
   $('#review_detail_home').css("margin-top", header_height);
+
+  if(window_width <= 420) {
+    // $('#email_detail_home').removeAttr('height');
+    $('#email_section').css("height",window_height);
+    $('#email_detail_home').css("height",window_height - header_height - 30);
+    $('#email_detail_home').css("margin-top", header_height);
+  } else {
+    $('#email_section').css("height",window_height - header_height * 2);
+    $('#email_detail_home').css("height",window_height - header_height * 4 - 30);
+    $('#email_detail_home').css("margin-top", header_height);
+  }
   
+
   if (document.getElementById("default-select")) {
     $("select").niceSelect();
   }
@@ -153,144 +165,6 @@ $(document).ready(function() {
       $("#header").removeClass("header-scrolled");
     }
   });
-
-  $(".active-post-carusel").owlCarousel({
-    items: 1,
-    loop: true,
-    margin: 0,
-    dots: false,
-    nav: true,
-    // autoplay: 2500,
-    navText: [
-      "<span class='lnr lnr-arrow-up'></span>",
-      "<span class='lnr lnr-arrow-down'></span>"
-    ]
-  });
-
-  //  Start Google map
-
-  // When the window has finished loading create our google map below
-
-  if (document.getElementById("map")) {
-    google.maps.event.addDomListener(window, "load", init);
-    function init() {
-      // Basic options for a simple Google Map
-      // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-      var mapOptions = {
-        // How zoomed in you want the map to start at (always required)
-        zoom: 11,
-        // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(40.67, -73.94), // New York
-        // How you would like to style the map.
-        // This is where you would paste any style found on Snazzy Maps.
-        styles: [
-          {
-            featureType: "water",
-            elementType: "geometry",
-            stylers: [{ color: "#e9e9e9" }, { lightness: 17 }]
-          },
-          {
-            featureType: "landscape",
-            elementType: "geometry",
-            stylers: [{ color: "#f5f5f5" }, { lightness: 20 }]
-          },
-          {
-            featureType: "road.highway",
-            elementType: "geometry.fill",
-            stylers: [{ color: "#ffffff" }, { lightness: 17 }]
-          },
-          {
-            featureType: "road.highway",
-            elementType: "geometry.stroke",
-            stylers: [{ color: "#ffffff" }, { lightness: 29 }, { weight: 0.2 }]
-          },
-          {
-            featureType: "road.arterial",
-            elementType: "geometry",
-            stylers: [{ color: "#ffffff" }, { lightness: 18 }]
-          },
-          {
-            featureType: "road.local",
-            elementType: "geometry",
-            stylers: [{ color: "#ffffff" }, { lightness: 16 }]
-          },
-          {
-            featureType: "poi",
-            elementType: "geometry",
-            stylers: [{ color: "#f5f5f5" }, { lightness: 21 }]
-          },
-          {
-            featureType: "poi.park",
-            elementType: "geometry",
-            stylers: [{ color: "#dedede" }, { lightness: 21 }]
-          },
-          {
-            elementType: "labels.text.stroke",
-            stylers: [
-              { visibility: "on" },
-              { color: "#ffffff" },
-              { lightness: 16 }
-            ]
-          },
-          {
-            elementType: "labels.text.fill",
-            stylers: [
-              { saturation: 36 },
-              { color: "#333333" },
-              { lightness: 40 }
-            ]
-          },
-          { elementType: "labels.icon", stylers: [{ visibility: "off" }] },
-          {
-            featureType: "transit",
-            elementType: "geometry",
-            stylers: [{ color: "#f2f2f2" }, { lightness: 19 }]
-          },
-          {
-            featureType: "administrative",
-            elementType: "geometry.fill",
-            stylers: [{ color: "#fefefe" }, { lightness: 20 }]
-          },
-          {
-            featureType: "administrative",
-            elementType: "geometry.stroke",
-            stylers: [{ color: "#fefefe" }, { lightness: 17 }, { weight: 1.2 }]
-          }
-        ]
-      };
-
-      // Get the HTML DOM element that will contain your map
-      // We are using a div with id="map" seen below in the <body>
-      var mapElement = document.getElementById("map");
-
-      // Create the Google Map using our element and options defined above
-      var map = new google.maps.Map(mapElement, mapOptions);
-
-      // Let's also add a marker while we're at it
-      var marker = new google.maps.Marker({
-        position: new google.maps.LatLng(40.67, -73.94),
-        map: map,
-        title: "Snazzy!"
-      });
-    }
-  }
-
-  $(document).ready(function() {
-    $("#mc_embed_signup")
-      .find("form")
-      .ajaxChimp();    
-  });
-
-  $(document).on("click", "#create_account", function(e) {
-    $('#signinModalForm').modal('hide');
-    $('#signupModalForm').modal('show');
-  });
-
-  $(document).on("click", "#signin_account", function(e) {
-    $('#signupModalForm').modal('hide');
-    $('#signinModalForm').modal('show');
-  });
-  
 });
 
 
