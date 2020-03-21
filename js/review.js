b235f89db471daa41649f5dbe20cb6d037bbcb85
prettypanda0720-ww <riverstar1992@gmail.com>
2020-03-21 04:06:44 +0100

    implemet review06,review07,review08 page

--- js/review.js
+++ js/review.js
@@ -160,6 +160,18 @@ $(document).ready(function() {
              hideValidate(input[i]);
            }
          }
+
+         input = $('.review-regarding .input200');
+         console.log(input.length);
+         for(var i=0; i<input.length; i++) {
+           if(validate(input[i]) == false){
+             // console.log('validate false');
+             showValidate(input[i]);
+           } else if(validate(input[i]) == true){
+             console.log('validate true');
+             hideValidate(input[i]);
+           }
+         }
       },
       rules: {
           feeling : {
@@ -187,6 +199,15 @@ $(document).ready(function() {
               required: true,
           },
           bus_operator : {
+              required: true,
+          },
+          quick_review : {
+              required: true,
+          },
+          event: {
+              required: true,
+          },
+          comment: {
               required: true,
           },
       },
@@ -274,6 +295,12 @@ $(document).ready(function() {
     });
   });
 
+  $('.review-regarding .input200').each(function(){
+    $(this).focus(function(){
+       hideValidate(this);
+    });
+  });
+
   jQuery.extend(jQuery.validator.messages, {
       required: "",
       remote: "",
@@ -291,6 +318,9 @@ $(document).ready(function() {
       route_no: "",
       route_name: "",
       bus_operator: "",
+      quick_review: "",
+      event: "",
+      comment: "",
   });
 
   $(document).ready(function(){
@@ -315,6 +345,71 @@ $(document).ready(function() {
       zoom:5,
     };
     var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+  }
+
+  $('#quick_review').parent().append('<ul  class="list-item" id="newpayment_type" name="quick_review"></ul>');
+  $('#quick_review option').each(function(){
+      $('#newpayment_type').append('<li value="' + $(this).val() + '' + '" data-symbol="' + '&#xf2f9;' + '">' + $(this).text()+'</li>');
+  });
+  $('#quick_review').remove();
+  $('#newpayment_type').attr('id', 'quick_review');
+  $('#quick_review li').first().addClass('init');
+  $("#quick_review").on("click", ".init", function() {
+      $(this).closest("#quick_review").children('li:not(.init)').toggle();
+  });
+  
+  var PaymentsOptions = $("#quick_review").children('li:not(.init)');
+  $("#quick_review").on("click", "li:not(.init)", function() {
+      PaymentsOptions.removeClass('selected');
+      $(this).addClass('selected');
+      $("#quick_review").children('.init').html($(this).html());
+      PaymentsOptions.toggle();
+  });
+
+  var imgUpload = document.getElementById('upload_imgs')
+    , imgPreview = document.getElementById('img_preview')
+    , imgUploadForm = document.getElementById('img-upload-form')
+    , totalFiles
+    , img;
+
+  imgUpload.addEventListener('change', previewImgs, false);
+  // imgUploadForm.addEventListener('submit', function (e) {
+  //   e.preventDefault();
+  //   alert('Images Uploaded! (not really, but it would if this was on your website)');
+  // }, false);
+
+  function previewImgs(event) {
+    
+    totalFiles = imgUpload.files.length;
+    
+    if(!!totalFiles) {
+      // imgPreview.classList.remove('quote-imgs-thumbs--hidden');
+      $(".active-post-carusel").remove();
+      $('.carousel-wrapper').append('<div class="owl-carousel active-post-carusel quote-imgs-thumbs quote-imgs-thumbs--hidden" id="img_preview" aria-live="polite"></div>');
+      var imgPreview = document.getElementById('img_preview');
+    }
+    
+
+    for(var i = 0; i < totalFiles; i++) {
+      img = document.createElement('img');
+      img.src = URL.createObjectURL(event.target.files[i]);
+      img.classList.add('img-preview-thumb');
+      imgPreview.appendChild(img);
+      console.log('imgs are added!');
+    }
+
+    $(".active-post-carusel").owlCarousel({
+        items: 3,
+        loop: true,
+        margin: 0,
+        dots: false,
+        nav: true,
+        // autoplay: 2500,
+        navText: [
+          // "<span class='lnr lnr-arrow-up'></span>",
+          // "<span class='lnr lnr-arrow-down'></span>"
+        ]
+    });
   }
 });
 
